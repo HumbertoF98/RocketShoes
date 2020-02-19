9ab9b238188ff3c8d9478b094d9219afbc881fcb
Humberto Filho <humbertofilho5@gmail.com>
2020-02-19 16:18:31 -0400

    estilização do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { MdRemoveCircleOutline, MdAddCircleOutline } from 'react-icons/md';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
+
 
 export default function Cart() {
   return (
@@ -27,12 +29,22 @@ export default function Cart() {
               <span>R$129,90</span>
             </td>
             <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
               <button type="button">
-                <MdRemoveCircleOutline size={20} color="#7159c1" />
-              </button>
-              <input type="number" readOnly value={1} />
-              <button type="button">
-                <MdAddCircleOutline size={20} color="#7159c1" />
+                <MdDelete size={20} color="#7159c1" />
               </button>
             </td>
           </tr>
