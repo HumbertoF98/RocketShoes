f985806b00da28a95437e584c77f08d4837fb3eb
Humberto Filho <humbertofilho5@gmail.com>
2020-02-19 17:54:48 -0400

    listando produto no carrinho

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -2,7 +2,11 @@ export default function cart(state = [], action) {
   // esse switch garante que o reducer só ouça a ação ADD_TO_CART, já que todos os reducers são ativados no dispatch
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, action.product];
+      return [...state, {
+        ...action.product,
+        amount: 1,
+      },
+      ];
     default:
       return state;
   }
