5658a1bb54d7bad35634ef4a0b1daaa2b2f4601a
Humberto Filho <humbertofilho5@gmail.com>
2020-02-21 12:01:02 -0400

    rocketshoes finalizado

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,11 +1,13 @@
 import { call, put, select, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
+
 import api from '../../../services/api';
+import history from '../../../services/history';
 
 import { formatPrice } from '../../../util/format';
 
-import { addToCartSucess, updateAmount } from './actions';
+import { addToCartSucess, updateAmountSuccess } from './actions';
 /* 
 * * = equivale a um async
 */
@@ -28,7 +30,7 @@ function* addToCart({ id }) {
 
 
   if (productExists) {
-    yield put(updateAmount(id, amount))
+    yield put(updateAmountSuccess(id, amount))
   }
   else {
     const response = yield call(api.get, `/products/${id}`);
@@ -40,9 +42,26 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSucess(data));
+    history.push('/cart');
+
   }
+}
+
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora de estoque');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
 }
 
 export default all([
   takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
 ]);
